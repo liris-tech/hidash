9ec59ae8f9273603411666e2bcc8ed92eb2261b6
zforro <zforro@protonmail.ch>
2023-12-28 17:20:33 +0100

    Update index.js and bump version to 0.1.0

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 import { diff } from './src/diff.js';
 
-export { diff };
+import { selectLines } from './src/selectLines.js';
+
+import { walkDir } from './src/walkDir.js';
+import { getSubDirs } from './src/getSubDirs.js';
+import { isDirEmpty } from './src/isDirEmpty.js';
+import { mkdirIfNotExists } from './src/mkdirIfNotExists.js';
+import { rmdirIfEmpty } from './src/rmdirIfEmpty.js';
+import { isDir } from './src/isDir.js';
+import { isSymbolicLink } from './src/isSymbolicLink.js';
+
+
+export {
+    diff,
+
+    selectLines,
+
+    walkDir,
+    getSubDirs,
+    isDirEmpty,
+    mkdirIfNotExists,
+    rmdirIfEmpty,
+    isDir,
+    isSymbolicLink
+};
