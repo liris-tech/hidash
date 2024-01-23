c12a14191a7d7eb480379119a88f5aa78c4b1fd8
zforro <zforro@protonmail.com>
2024-01-23 17:18:06 +0100

    Update index.js and bump version to 0.1.1

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import { diff } from './src/diff.js';
 
 import { selectLines } from './src/selectLines.js';
 
-import { walkDir } from './src/walkDir.js';
+import { makeFileStructure } from './src/makeFileStructure.js';
+import { matchesFileStructure } from './src/matchesFileStructure.js';
+import { isDir } from './src/isDir.js';
+import { isSymbolicLink } from './src/isSymbolicLink.js';
 import { getSubDirs } from './src/getSubDirs.js';
 import { isDirEmpty } from './src/isDirEmpty.js';
 import { mkdirIfNotExists } from './src/mkdirIfNotExists.js';
 import { rmdirIfEmpty } from './src/rmdirIfEmpty.js';
-import { isDir } from './src/isDir.js';
-import { isSymbolicLink } from './src/isSymbolicLink.js';
+import { walkDir } from './src/walkDir.js';
 
 
 export {
@@ -16,11 +18,13 @@ export {
 
     selectLines,
 
-    walkDir,
+    makeFileStructure,
+    matchesFileStructure,
+    isDir,
+    isSymbolicLink,
     getSubDirs,
     isDirEmpty,
     mkdirIfNotExists,
     rmdirIfEmpty,
-    isDir,
-    isSymbolicLink
+    walkDir
 };
